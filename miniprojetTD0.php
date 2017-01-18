4511eabbda719c70254c5e723e933f8201f1adca
MonsieurJose <21505847@etu.unicaen.fr>
2017-01-18 15:13:16 +0100

    TD0_v2

--- miniprojetTD0.php
+++ miniprojetTD0.php
@@ -1,15 +1,43 @@
 <?php
 function creerConnexionBD(){
 	try{	
-		$bd = new PDO('mysql:host=localhost;dbname=;charset=utf8', 'root', '');
+		$bd = new PDO('mysql:host=localhost;dbname=phalcon-td0;charset=utf8', 'root', '');
 		}
 		catch (Exception $e)
 		{
 			die('Erreur : ' . $e->getMessage());
 		}
-	return $bd;	
-}
-echo "Bonjour les gens";
-commit;
-push;
+	   return $bd;	
+    }
+    $bd = creerConnexionBD();
+    $req2 = "select name,count(role.id)as nbuti from role join user on role.id=user.idrole group by role.id order by 1;";
+    $reponse2 = $bd->query($req2);
+    $nbUtilisateur = 0; 
+    ?>
+    <label for="tri_nom"></label>
+		<input type="submit" value="tri_nom" name="true" />
+    <label for="tri_nb_utilisateur"></label>
+		<input type="submit" value="tri_nb_utilisateur" name="true" />
+
+
+
+
+
+
+
+
+
+
+    <?php
+    echo "</br>";
+    echo "</br>";
+
+    foreach ($reponse2 as $valeur2) {
+        echo "--- ".$valeur2['name']." --> ".$valeur2['nbuti']."</br>";
+        $nbUtilisateur = $nbUtilisateur+1; 
+    }       
+    echo "</br>";
+    echo "le nombre de roles est : ".$nbUtilisateur."</br>";
+
+
 ?>
